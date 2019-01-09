84f7d4577ba39192bc11c60f86b07361ce930334
Miroslav Krýsl <wilyxjam@gmail.com>
2019-01-09 15:08:09 +0100

    implement Stats class

--- Stats.cpp
+++ Stats.cpp
@@ -1,1 +1,81 @@
+#include <sstream>
+
 #include "Stats.h"
+
+Stats::Stats()
+    : started(std::chrono::system_clock::now()),
+      messagesReceived{0},
+      messagesDropped{0},
+      bytesReceived{0},
+      bytesDropped{0},
+      messagesSent{0},
+      bytesSent{0}
+{}
+
+void Stats::setStarted(const std::chrono::system_clock::time_point &started)
+{
+    Stats::started = started;
+    std::unique_lock<std::mutex>
+}
+
+void Stats::addMessagesReceived(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{messagesReceivedMutex};
+    messagesReceived += count;
+}
+
+void Stats::addMessagesDropped(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{messagesDroppedMutex};
+    messagesDropped += count;
+}
+
+void Stats::addBytesReceived(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{bytesReceivedMutex};
+    bytesReceived += count;
+}
+
+void Stats::addBytesDropped(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{bytesDroppedMutex};
+    bytesDropped += count;
+}
+
+void Stats::addMessagesSent(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{messagesSentMutex};
+    messagesSent += count;
+}
+
+void Stats::addBytesSent(uint64_t count)
+{
+    std::unique_lock<std::mutex> lock{bytesSentMutex};
+    bytesSent += count;
+}
+
+std::string Stats::toLogString()
+{
+    auto now = std::chrono::system_clock::now();
+    std::time_t startTime = std::chrono::system_clock::to_time_t(started);
+    auto upTime = now - started;
+
+    std::stringstream stream;
+
+    stream << "Server started at: " << std::ctime(&startTime) << std::endl;
+    stream << "Already running: "
+           << std::chrono::duration_cast<std::chrono::hours>(upTime).count() << " hours "
+           << std::chrono::duration_cast<std::chrono::minutes>(upTime).count() << " minutes "
+           << std::chrono::duration_cast<std::chrono::seconds>(upTime).count() << " seconds"
+           << std::endl;
+    stream << "Bytes received: " << bytesReceived + bytesDropped << std::endl;
+    stream << "Messages received: " << messagesReceived + messagesDropped << std::endl;
+    stream << std::endl;
+    stream << "Bytes sent: " << bytesSent << std::endl;
+    stream << "Messages sent: " << messagesSent << std::endl;
+    stream << std::endl;
+    stream << "Messages dropped: " << bytesDropped << std::endl;
+    stream << "Bytes dropped: " << messagesDropped << std::endl;
+
+    return stream.str();
+}
--- Stats.h
+++ Stats.h
@@ -1,9 +1,37 @@
 #pragma once
 
 #include <string>
+#include <chrono>
+#include <mutex>
+
 class Stats
 {
+    std::chrono::system_clock::time_point started;
+    uint64_t messagesReceived;
+    uint64_t messagesDropped;
+    uint64_t bytesReceived;
+    uint64_t bytesDropped;
+    uint64_t messagesSent;
+    uint64_t bytesSent;
+
+    std::mutex messagesReceivedMutex;
+    std::mutex messagesDroppedMutex;
+    std::mutex bytesReceivedMutex;
+    std::mutex bytesDroppedMutex;
+    std::mutex messagesSentMutex;
+    std::mutex bytesSentMutex;
+
 public:
+    Stats();
+
+    void setStarted(const std::chrono::system_clock::time_point &started);
+    void addMessagesReceived(uint64_t count);
+    void addMessagesDropped(uint64_t count);
+    void addBytesReceived(uint64_t count);
+    void addBytesDropped(uint64_t count);
+    void addMessagesSent(uint64_t count);
+    void addBytesSent(uint64_t count);
+
     std::string toLogString();
 };
 
