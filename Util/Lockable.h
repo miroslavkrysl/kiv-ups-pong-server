99691281eab99164f8b3e65c4be0fb3f68ea9046
Miroslav Krýsl <wilyxjam@gmail.com>
2019-01-17 15:16:14 +0100

    make mutex in Lockable recursive

--- Util/Lockable.h
+++ Util/Lockable.h
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <condition_variable>
+#include <mutex>
 #include <chrono>
 
 typedef std::unique_lock<std::mutex> Lock;
@@ -13,7 +14,7 @@ typedef std::cv_status WaitStatus;
 class Lockable
 {
     mutable std::condition_variable conditionVariable;
-    mutable std::mutex mutex;
+    mutable std::recursive_mutex mutex;
 
 public:
     virtual Lock acquireLock() const final;
