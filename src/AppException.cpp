f638f5a2cc2acc869c0da963cdd0c6a6fed79597
Miroslav Krýsl <wilyxjam@gmail.com>
2019-03-05 09:26:06 +0100

    add AppException

--- src/AppException.cpp
+++ src/AppException.cpp
@@ -1,1 +1,5 @@
 #include "AppException.h"
+
+AppException::AppException(const std::string &message)
+    : std::runtime_error{message}
+{}
--- src/AppException.h
+++ src/AppException.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include <bits/exception.h>
+#include <stdexcept>
 
 /**
  * The base application exception class.
  */
-class AppException: public std::exception
+class AppException: public std::runtime_error
 {
-
+public:
+    /**
+     * Initialize a new AppException with the given message.
+     *
+     * @param message The exception message.
+     */
+    explicit AppException(const std::string &message);
 };
 
 
